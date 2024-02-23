f4e09fee16cabd704f0db19d738fd373f212592e
Gins-07 <73919860+Gins-07@users.noreply.github.com>
2024-02-23 15:45:41 +0000

    Add files via upload

---
+++ Train/train_xr.py
@@ -0,0 +1,70 @@
+
+import sys
+sys.path.append('../')
+
+import torch
+import time
+from Config.gan_xr_options import gan_xr_v1,gan_xr_v2,gan_xrp_v5
+from Data import CreateDataLoader
+from Model import create_model
+from Utils.visualizer import Visualizer
+import numpy as np
+
+
+opt = gan_xr_v1()
+data_loader = CreateDataLoader(opt)
+
+
+dataset = data_loader.load_data()
+dataset_size = len(data_loader)
+print('#training images = %d' % dataset_size)
+
+model = create_model(opt)
+visualizer = Visualizer(opt)
+total_steps = 0
+
+if __name__ == "__main__":
+#     freeze_support()
+# In[ ]:
+    for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
+        epoch_start_time = time.time()
+        iter_data_time = time.time()
+        epoch_iter = 0
+
+        for i, data in enumerate(dataset):
+            iter_start_time = time.time()
+            if total_steps % opt.print_freq == 0:
+                t_data = iter_start_time - iter_data_time
+            visualizer.reset()
+            total_steps += opt.batchSize
+            epoch_iter += opt.batchSize
+            model.set_input(data)
+            model.optimize_parameters()
+
+            if total_steps % opt.display_freq == 0:
+                save_result = total_steps % opt.update_html_freq == 0
+                visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
+
+            if total_steps % opt.print_freq == 0:
+                errors = model.get_current_errors()
+                t = (time.time() - iter_start_time) / opt.batchSize
+                visualizer.print_current_errors(epoch, epoch_iter, errors, t, t_data)
+                if opt.display_id > 0:
+                    visualizer.plot_current_errors(epoch, float(epoch_iter) / dataset_size, opt, errors)
+
+            if total_steps % opt.save_latest_freq == 0:
+                print('saving the latest model (epoch %d, total_steps %d)' %
+                      (epoch, total_steps))
+                model.save('latest')
+
+            iter_data_time = time.time()
+        if epoch % opt.save_epoch_freq == 0:
+            print('saving the model at the end of epoch %d, iters %d' %
+                  (epoch, total_steps))
+            model.save('latest')
+            model.save(epoch)
+
+        print('End of epoch %d / %d \t Time Taken: %d sec' %
+              (epoch, opt.niter + opt.niter_decay, time.time() - epoch_start_time))
+        model.update_learning_rate()
+
